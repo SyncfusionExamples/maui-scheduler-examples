f4bc4d1a84511f4a929dd9cf50c89afbd49463e0
Karthick Mani <karthick.mani@syncfusion.com>
2021-12-21 12:07:19 +0530

    Integrated getting started demo.

---
+++ GettingStarted/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace GettingStarted
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="MainPage" /> class.
+            /// </summary>
+            InitializeComponent();
+        }
+    }
+}
