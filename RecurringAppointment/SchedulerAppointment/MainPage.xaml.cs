580d4d2ff3f6893fdd9e114dc479876063bbce92
Karthick Mani <karthick.mani@syncfusion.com>
2021-12-21 15:11:49 +0530

    Integrated recurring-appointments demo

---
+++ RecurringAppointment/SchedulerAppointment/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace RecurringSchedulerAppointment
+{
+	/// <summary>
+	/// Interaction logic for MainPage.xaml
+	/// </summary>
+	public partial class MainPage : ContentPage
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="MainPage" /> class.
+		/// </summary>
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
