996dfaced893dddbf6cdee82e4612984139a116d
Karthick Mani <karthick.mani@syncfusion.com>
2021-12-21 19:05:18 +0530

    Integrated recursive-exception-appointment demo

---
+++ RecursiveExceptionAppointment/SchedulerAppointment/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Controls.Hosting;
+using Syncfusion.Maui.Core.Hosting;
+
+namespace RecurringSchedulerAppointment
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+
+            builder.ConfigureSyncfusionCore();
+            builder
+            .UseMauiApp<App>().ConfigureMauiHandlers(handlers =>
+            {
+
+
+            })
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("Segoe-mdl2.ttf", "SegoeMDL2");
+            });
+
+            return builder.Build();
+        }
+    }
+}
