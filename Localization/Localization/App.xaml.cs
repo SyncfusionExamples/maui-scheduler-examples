9d03db715c546da2213a7edf991a4e260fd5e595
Indumathi R <indumathi.ravichandran@syncfusion.com>
2022-04-11 19:06:05 +0530

    ViewHeaderTemplateSample-Sample integrated.

---
+++ Localization/Localization/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Syncfusion.Maui.Scheduler;
+using System.Globalization;
+using System.Resources;
+
+namespace Localization;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+        CultureInfo.CurrentUICulture = new CultureInfo("fr-FR");
+
+        SfSchedulerResources.ResourceManager = new ResourceManager("Localization.Resources.SfScheduler", Application.Current.GetType().Assembly);
+
+        MainPage = new MainPage();
+	}
+}
