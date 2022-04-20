42e2701fc27684b9840b03b7fbd108b1e7c24301
Indumathi R <indumathi.ravichandran@syncfusion.com>
2022-04-20 15:44:22 +0530

    ViewHeaderTemplateSample-Month cell template selector sample integrated.

---
+++ MonthCellTemplateSelector/MonthCellTemplateSelector/MonthCellTemplateSelector.cs
@@ -0,0 +1,26 @@
+﻿using Syncfusion.Maui.Scheduler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonthCellTemplateSelector
+{
+    public class MonthCellTemplateSelector : DataTemplateSelector
+    {
+        public MonthCellTemplateSelector()
+        {
+        }
+        public DataTemplate NormalDateTemplate { get; set; }
+        public DataTemplate TodayDateTemplate { get; set; }
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var monthCellDetails = item as SchedulerMonthCellDetails;
+            if (monthCellDetails.DateTime.Date == DateTime.Today.Date)
+                return TodayDateTemplate;
+            else
+                return NormalDateTemplate;
+        }
+    }
+}
