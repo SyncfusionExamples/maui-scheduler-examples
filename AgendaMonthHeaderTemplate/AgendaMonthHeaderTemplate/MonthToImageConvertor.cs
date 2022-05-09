fc8836f105423701896726a110eac038ba44503b
Indumathi R <indumathi.ravichandran@syncfusion.com>
2022-05-09 17:23:58 +0530

    KBSample-Sample integrated for agenda month header template.

---
+++ AgendaMonthHeaderTemplate/AgendaMonthHeaderTemplate/MonthToImageConvertor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaMonthHeaderTemplate
+{
+    internal class MonthToImageConvertor : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                var monthName = String.Format("{0:MMMM}", value).ToLower();
+                return monthName + ".png";
+            }
+
+            return null;
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
