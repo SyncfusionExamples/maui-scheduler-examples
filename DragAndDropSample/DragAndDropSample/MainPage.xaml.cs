53938996f154321fc33143a0806fee47b3639762
Kerubananthan-Ganesan <104969840+Kerubananthan3761@users.noreply.github.com>
2023-06-23 13:42:33 +0530

    Added drag and drop sample

---
+++ DragAndDropSample/DragAndDropSample/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Syncfusion.Maui.Scheduler;
+using System.Collections.ObjectModel;
+
+namespace DragAndDropSample;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+    }
+
+}
+
